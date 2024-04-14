23c72ea3d2ce102ae68bf90e69ba70dc4bd40584
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-15 01:30:14 +0530

    Ready to deploy

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -4,7 +4,7 @@ const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      axios.post('http://localhost:3001/authenticate',
+      axios.post('https://chatify-react-node-2.onrender.com/authenticate',
         {username: value})
         .then(r => props.onAuth({...r.data, secret: value}))
         .catch(e => console.log('error', e))
