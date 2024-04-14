ee9b241a19fc81f0984b2091ef3a148abde628bf
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-15 01:41:36 +0530

    Ready to deploy

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div className="background">
       <PrettyChatWindow
-        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID || "2c784d4b-5b00-47d0-be64-2e2b6618275d"}
+        projectId={import.meta.env.CHAT_ENGINE_PROJECT_ID || "2c784d4b-5b00-47d0-be64-2e2b6618275d"}
         username={props.user.username}
         secret={props.user.secret}
       />
