3c77c840882ff9b59d9360afb8d5e14be9b10c86
Pavith Priyadharsan S <146210938+PavithPriyadharsan@users.noreply.github.com>
2024-04-16 15:08:52 +0530

    Ready to deploy

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,15 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID || "2c784d4b-5b00-47d0-be64-2e2b6618275d"}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
